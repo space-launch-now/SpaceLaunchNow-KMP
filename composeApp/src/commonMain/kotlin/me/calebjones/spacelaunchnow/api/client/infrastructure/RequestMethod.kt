b61f0515aaeaf3f646e87a9f0cf8a353a7988f02
Caleb Jones <ca.jones9119@gmail.com>
2025-08-27 22:56:42 -0400

    another checkpoint... 2.4.0 working

--- composeApp/src/commonMain/kotlin/me/calebjones/spacelaunchnow/api/client/infrastructure/RequestMethod.kt
+++
@@ -1,8 +0,0 @@
-package me.calebjones.spacelaunchnow.api.client.infrastructure
-
-/**
- * Provides enumerated HTTP verbs
- */
-enum class RequestMethod {
-    GET, DELETE, HEAD, OPTIONS, PATCH, POST, PUT
-}
