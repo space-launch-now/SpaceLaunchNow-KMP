6502f9f355d694ea9705b6e54fe6c56a87e6ebda
Caleb Jones <>
2025-09-08 11:38:08 -0400

    iOS checkpoint

--- composeApp/src/iosMain/kotlin/me/calebjones/spacelaunchnow/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/me/calebjones/spacelaunchnow/MainViewController.kt
@@ -2,4 +2,4 @@ package me.calebjones.spacelaunchnow
 
 import androidx.compose.ui.window.ComposeUIViewController
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController { SpaceLaunchNowApp() }
