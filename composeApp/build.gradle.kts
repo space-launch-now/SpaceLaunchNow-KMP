77699602dc9be097c3fa47d670995dfaa9b51292
Caleb Jones <ca.jones9119@gmail.com>
2025-10-29 14:12:48 -0400

    fix: enable AD_FREE feature in PremiumFeature

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -243,8 +243,6 @@ android {
 
     sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
     sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-
 
     defaultConfig {
         applicationId = "me.calebjones.spacelaunchnow"
